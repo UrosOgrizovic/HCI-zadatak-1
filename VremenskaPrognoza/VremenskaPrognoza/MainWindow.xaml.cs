3aa320675e232500729260ceb5fc1fde58356100
UrosOgrizovic <uros.ogrizovic671@gmail.com>
2019-03-31 18:44:05 +0200

    Added fetching temperature with basic display

--- VremenskaPrognoza/VremenskaPrognoza/MainWindow.xaml.cs
+++ VremenskaPrognoza/VremenskaPrognoza/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace VremenskaPrognoza
 {
@@ -20,9 +22,30 @@ namespace VremenskaPrognoza
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		const string APPID = "66382f7d97c7e80060fb8451446e6ffc";
+		string cityName = "Belgrade";
 		public MainWindow()
 		{
 			InitializeComponent();
+			getWeather(cityName, APPID);
+		}
+		void getWeather(string city, string appid)
+		{
+			using (WebClient web = new WebClient())
+			{
+				string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ appid + "&units=metric&cnt=6");
+
+				var json = web.DownloadString(url);
+				var result = JsonConvert.DeserializeObject<WeatherInfo.Root>(json);
+
+				WeatherInfo.Root output = result;
+
+				lbl_cityName.Content = string.Format("{0}", output.name);
+				lbl_country.Content = string.Format("{0}", output.sys.country);
+				lbl_temp.Content = string.Format("{0} \u00B0"+ "C", output.main.temp);
+
+			}
+				
 		}
 	}
 }
---
+++ VremenskaPrognoza/VremenskaPrognoza/WeatherInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VremenskaPrognoza
+{
+	class WeatherInfo
+	{
+		public class coord
+		{
+			public double ion { get; set; }
+			public double lat { get; set; }
+		}
+
+		public class weather
+		{
+			public int id { get; set; }
+			public string main { get; set; }
+			public string description { get; set; }
+		}
+
+		public class main
+		{
+			public double temp { get; set; }
+			public double pressure { get; set; }
+			public double humidity { get; set; }
+		}
+
+		public class wind
+		{
+			public double speed { get; set; }
+		}
+
+		public class sys
+		{
+			public string country { get; set; }
+		}
+
+		public class Root
+		{
+			public string name { get; set; }
+			public sys sys { get; set; }
+			public double dt { get; set; }
+			public wind wind { get; set; }
+			public main main { get; set; }
+			public List<weather> weatherList { get; set; }
+			public coord coordinate { get; set; }
+
+		}
+	}
+}
